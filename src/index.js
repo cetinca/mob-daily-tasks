7144587559eea6633078b837273f3015baf55b10
Cetin CAKIR <cetincakirtr@gmail.com>
2024-04-12 15:05:15 +0300

    feat: initial

---
+++ src/index.js
@@ -0,0 +1,65 @@
+const inputEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
+const notListEl = document.getElementById("note-list")
+
+
+addButtonEl.addEventListener("click", addNote)
+
+
+function generateUniqueID() {
+    return crypto.randomUUID();
+  }
+
+function setNotes(notes) {
+    localStorage.setItem("notes", JSON.stringify(notes))
+}
+
+function getNotes() {
+    return JSON.parse(localStorage.getItem("notes")) || []
+}
+
+function addNote() {
+    let inputValue = inputEl.value
+    if (!inputValue) {
+        inputEl.focus()
+        return
+    }
+    const notList = getNotes()
+    const note = { id: generateUniqueID(), text: inputEl.value }
+    notList.push(note)
+    setNotes(notList)
+    renderNotes(notList)
+    inputEl.value = ""
+}
+
+
+function deleteNote(event) {
+    const liId = event.target.id
+    const notes = getNotes()
+    const newNotes = []
+    notes.map(note => {
+        liId !== note.id && newNotes.push(note)
+    })
+    setNotes(newNotes)
+    renderNotes(newNotes)
+}
+
+inputEl.addEventListener("keypress", (event) => {
+    event.key === "Enter" &&  addNote();
+      })
+
+function renderNotes(notes) {
+    inputEl.focus()
+    notListEl.innerHTML = ""
+    notes.map(note => {
+        const noteLi = document.createElement("li")
+        noteLi.id = note.id
+        noteLi.innerText = note.text
+        noteLi.className = "note"
+        noteLi.onclick = deleteNote
+        console.log(noteLi)
+        notListEl.append(noteLi)
+    })
+}
+
+renderNotes(getNotes())
